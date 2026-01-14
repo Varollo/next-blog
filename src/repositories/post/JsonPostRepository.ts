8306366a2e731306993687ef4dbe9897e7bb1e19
Gabriel Carollo <rimoldi98@gmail.com>
2026-01-14 14:41:23 -0300

    Added post repository

---
+++ src/repositories/post/JsonPostRepository.ts
@@ -0,0 +1,29 @@
+import { PostModel } from "@/models/post/PostModel";
+import { PostRepository } from './PostRepository';
+import { resolve } from "path";
+import { readFile } from "fs/promises";
+
+const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(ROOT_DIR, "src", "db", "seed", "posts.json");
+
+export class JsonPostRepository implements PostRepository {
+  private async readFromDisc(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, 'utf-8');
+    return JSON.parse(jsonContent).posts;
+  }
+
+  async findAll(): Promise<PostModel[]> {
+    return await this.readFromDisc();
+  }
+
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find(post => post.id === id);
+
+    if (!post) throw new Error(`Post with id '${id}' was not found.`);
+
+    return post;
+  }
+}
+
+export const postRepository: PostRepository = new JsonPostRepository();
