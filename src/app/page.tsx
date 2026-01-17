f72e1c820418b13a03ff814a2bbcf749cdafb60e
Gabriel Carollo <rimoldi98@gmail.com>
2026-01-17 14:05:13 -0300

    Added header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
@@ -6,6 +7,8 @@ import { Suspense } from "react";
 export default async function HomePage() {
   return (
     <Container>
+      <Header />
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
